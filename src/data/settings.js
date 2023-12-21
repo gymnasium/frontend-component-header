7cdb8b1c90f1c6b2c6af6efeff373733f4c654eb
Roman Edirisinghe <rediris@users.noreply.github.com>
2023-12-21 13:20:08 -0500

    Update settings.js

--- src/data/settings.js
+++ src/data/settings.js
@@ -1,5 +1,8 @@
+import { getConfig } from '@edx/frontend-platform';
+
 export default async function gymSettings() {
   try {
+    const config = getConfig();
     let response = await fetch(`${config.MARKETING_SITE_BASE_URL}/feeds/config.json`);
     let responseJson = await response.json();
     return responseJson;
