3fa5bac84f203a8c8eeff43caff549cdf4aa4f1c
Roman Edirisinghe <rediris@users.noreply.github.com>
2023-12-28 13:28:03 -0500

    Update CoursesNav.jsx

--- src/gym-header/CoursesNav.jsx
+++ src/gym-header/CoursesNav.jsx
@@ -3,12 +3,11 @@ import GymSettings from '../data/settings';
 const settings = await GymSettings();
 
 const CoursesNav = ({ intl }) => (
-  <li><a href="${settings.root_url}/courses/full/">Full Courses</a></li>
-  // settings.navigation.courses.map(item => {
-  //   return (
-  //     <li><a key={`item-${item.title}`} href={item.href}>{item.title}</a></li>
-  //   );
-  // })
+  settings.navigation.courses.map(item => {
+    return (
+      <li><a key={`item-${item.title}`} href={item.href}>{item.title}</a></li>
+    );
+  })
 );
 
 CoursesNav.propTypes = {
