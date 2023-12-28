3d501313f23e68964bbe4dac92fed729fcbbfd86
Roman Edirisinghe <rediris@users.noreply.github.com>
2023-12-28 13:28:10 -0500

    Update AccountNav.jsx

--- src/gym-header/AccountNav.jsx
+++ src/gym-header/AccountNav.jsx
@@ -4,16 +4,14 @@ import PropTypes from 'prop-types';
 import { getConfig } from '@edx/frontend-platform';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 
-import messages from './messages';
-
 const AccountNav = ({ intl, username }) => {
   return (
     <ul>
       <li><a href={`${getConfig().ACCOUNT_PROFILE_URL}/u/${username}`}>
-        {intl.formatMessage(messages.profile)}
+        Profile
       </a></li>
       <li><a href={getConfig().ACCOUNT_SETTINGS_URL}>
-        {intl.formatMessage(messages.account)}
+        Account
       </a></li>
     </ul>
   );
